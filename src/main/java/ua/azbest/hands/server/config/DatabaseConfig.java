a03e3ac1c597171752572a0d97ae561906f324a8
powerex <flaxazbest@gmail.com>
2016-04-09 02:01:32 +0300

    add connect to Database

--- src/main/java/ua/azbest/hands/server/config/DatabaseConfig.java
+++ src/main/java/ua/azbest/hands/server/config/DatabaseConfig.java
@@ -8,8 +8,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
@@ -49,7 +51,6 @@ public class DatabaseConfig {
         ds.setUsername(env.getRequiredProperty("db.username"));
         ds.setPassword(env.getRequiredProperty("db.password"));
 
-/*
         ds.setInitialSize(Integer.valueOf(env.getRequiredProperty("db.initialSize")));
         ds.setMinIdle(Integer.valueOf(env.getRequiredProperty("db.minIdle")));
         ds.setMaxIdle(Integer.valueOf(env.getRequiredProperty("db.maxIdle")));
@@ -57,7 +58,7 @@ public class DatabaseConfig {
         ds.setMinEvictableIdleTimeMillis(Long.valueOf(env.getRequiredProperty("db.minEvictableIdleTimeMillis")));
         ds.setTestOnBorrow(Boolean.valueOf(env.getRequiredProperty("db.testOnBorrow")));
         ds.setValidationQuery(env.getRequiredProperty("db.validationQuery"));
-*/
+
         return ds;
     }
 
@@ -71,5 +72,13 @@ public class DatabaseConfig {
         } catch (IOException e) {
             throw new IllegalArgumentException("Can't find 'hibernate.properties' in classpath!", e);
         }
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager manager = new JpaTransactionManager();
+        manager.setEntityManagerFactory(entityManagerFactory().getObject());
+
+        return manager;
     }
 }
